81b0683e8d49dee2e064f9d60b96f2d46a4fbde7
fvn70 <fvn_reg@mail.ru>
2022-03-08 16:34:07 +0700

    Stage 2/5: Easy does it

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 import re
+from random import shuffle
 
 win_combos = (
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -42,8 +43,7 @@ def draw(board):
 def analyze(board):
     win = get_winner(board)
     if win is None:
-        print("Game not finished")
-        return False
+        return True
     if win in ('X', 'O'):
         print(win + " wins")
     else:
@@ -51,26 +51,45 @@ def analyze(board):
     return False
 
 
-t = input("Enter cells: ")
-draw(t)
-isXgo = t.count('X') == t.count('O')
-while True:
-    digs = input("Enter the coordinates: ")
-    if not re.match("[1-9] [1-9]", digs):
-        print("You should enter numbers!")
-    elif not re.match("[1-3] [1-3]", digs):
-        print("Coordinates should be from 1 to 3!")
-    else:
-        row, col = digs.split()
-        k = 3 * (int(row) - 1) + int(col) -1
-        if t[k] == '_':
-            ch = "X" if isXgo else "O"
-            t = t[:k] + ch + t[k + 1:]
-            isXgo = not isXgo
-        else:
-            print("This cell is occupied! Choose another one!")
-            continue
+def next_comp(t, ch):
+    moves = [i for i, c in enumerate(t) if c == '_']
+    shuffle(moves)
+    k = moves[0]
+    t = t[:k] + ch + t[k + 1:]
+    print('Making move level "easy"')
+    draw(t)
+    return t
 
-        draw(t)
-        analyze(t)
-        break
+
+def next_user(t, ch):
+    while True:
+        digs = input("Enter the coordinates: ")
+        if not re.match("[1-9] [1-9]", digs):
+            print("You should enter numbers!")
+        elif not re.match("[1-3] [1-3]", digs):
+            print("Coordinates should be from 1 to 3!")
+        else:
+            row, col = digs.split()
+            k = 3 * (int(row) - 1) + int(col) -1
+            if t[k] == '_':
+                # ch = "X" if isXgo else "O"
+                t = t[:k] + ch + t[k + 1:]
+                # isXgo = not isXgo
+                draw(t)
+                return t
+            else:
+                print("This cell is occupied! Choose another one!")
+
+
+def main():
+    t_board = "_________"
+    draw(t_board)
+    while True:
+        t_board = next_user(t_board, 'X')
+        if not analyze(t_board):
+            break
+        t_board = next_comp(t_board, 'O')
+
+
+if __name__ == "__main__":
+    main()
